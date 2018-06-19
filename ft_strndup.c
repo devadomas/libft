8ef659efc4548b6512d05e270f22ef55a7c06d64
Adomas ZALIAUSKAS <azaliaus@student.42.fr>
2018-06-19 17:28:28 +0200

    ft_strndup fixed

--- ft_strndup.c
+++ ft_strndup.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/06/18 11:15:47 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/06/18 11:28:06 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/06/19 17:26:17 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,6 +20,6 @@ char		*ft_strndup(const char *str, size_t n)
 		return (NULL);
 	if (!(ret = ft_strnew(n)))
 		return (NULL);
-	ret = ft_strncpy(str, ret, n);
+	ret = ft_strncpy(ret, str, n);
 	return (ret);
 }
